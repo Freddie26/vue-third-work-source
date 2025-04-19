1829439aa958a5dca52d540507cfb905c3d2611c
ianatskiy <igor.anatsky@waveaccess.ru>
2025-04-19 13:49:04 +0300

    step-1.31: Добавлена логика в DemoCounter

--- course/section0/demo/src/demo0/DemoCounter.vue
+++ course/section0/demo/src/demo0/DemoCounter.vue
@@ -1,13 +1,18 @@
 <template>
   <div>
-    <button> - </button>
-    <span> Current value: 0 </span>
-    <button> + </button>
+    <button @click.prevent="decrement"> - </button>
+    <span> Current value: {{ count }} </span>
+    <button @click.prevent="increment"> + </button>
   </div>
 </template>
 
 <script setup>
+  import {ref} from "vue";
 
+  const count = ref(0)
+
+  const increment = () => { count.value += 1 }
+  const decrement = () => { count.value -= 1 }
 </script>
 
 <style scoped lang="scss">
