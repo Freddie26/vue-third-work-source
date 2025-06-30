e090c064fb12bdbeea36c8f62aa3e2e321c05525
oksmina <anaom@csgo.com>
2025-06-30 20:45:02 +0300

    step-3.4: Сброс наработки

---
+++ course/section2/frontend/src/common/validator.js
@@ -0,0 +1,44 @@
+import { emailRegex, urlRegex } from "./constants";
+import { isRef } from "vue";
+
+const rules = {
+  required: {
+    rule: (value) => {
+      if (isRef(value)) return !!value.value.trim();
+      return !!value?.trim();
+    },
+    message: "Поле обязательно для заполнения",
+  },
+  email: {
+    rule: (value) => {
+      if (isRef(value)) {
+        value = value.value;
+      }
+      return value ? emailRegex.test(String(value).toLowerCase()) : true;
+    },
+    message: "Электронная почта имеет неверный формат",
+  },
+  url: {
+    rule: (value) => {
+      if (isRef(value)) {
+        value = value.value;
+      }
+      return value ? urlRegex.test(value) : true;
+    },
+    message: "Ссылка имеет неверный формат",
+  },
+};
+
+const validate = (value, appliedRules) => {
+  let error = "";
+  appliedRules.forEach((appliedRule) => {
+    if (!rules[appliedRule]) {
+      return;
+    }
+    const { rule, message } = rules[appliedRule];
+    if (!rule(value)) {
+      error = message;
+    }
+  });
+  return error;
+};
