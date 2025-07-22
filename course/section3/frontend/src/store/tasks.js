2bb6ddb1cf365cb675ef180f3fca81640c0608b1
ianatskiy <igor.anatsky@waveaccess.ru>
2025-07-23 01:15:44 +0300

    step-4.6: Инициализация хранилища Pinia

---
+++ course/section3/frontend/src/store/tasks.js
@@ -0,0 +1,10 @@
+import { defineStore } from "pinia";
+
+export const useTasksStore = defineStore(
+  'tasks',
+  {
+    state: () => ({}),
+    getters: {},
+    actions: {},
+  }
+);
---
+++ course/section3/frontend/src/store/users.js
@@ -0,0 +1,10 @@
+import { defineStore } from "pinia";
+
+export const useUsersStore = defineStore(
+  'users',
+  {
+    state: () => ({}),
+    getters: {},
+    actions: {},
+  },
+);
