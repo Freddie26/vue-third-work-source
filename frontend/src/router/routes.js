a9f2c4ec738bb339148b6bc195624a5301506342
ianatskiy <igor.anatsky@waveaccess.ru>
2025-08-02 22:50:09 +0300

    step-5.7: Поддержан механизм guard-ов

--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -1,3 +1,5 @@
+import { isAdmin } from "./guards";
+
 export default [
 	{
 		path: '/login',
@@ -21,13 +23,19 @@ export default [
 				path: '/tasks/create',
 				name: 'TaskCreate',
 				component: () => import('../views/TaskCreate.vue'),
-				meta: { layout: 'AppLayoutMain' }
+				meta: {
+					layout: 'AppLayoutMain',
+					guards: [isAdmin],
+				}
 			},
 			{
 				path: '/tasks/edit/:id',
 				name: 'TaskEdit',
 				component: () => import('../views/TaskEdit.vue'),
-				meta: { layout: 'AppLayoutMain' }
+				meta: {
+					layout: 'AppLayoutMain',
+					guards: [isAdmin],
+				}
 			}
 		]
 	}
